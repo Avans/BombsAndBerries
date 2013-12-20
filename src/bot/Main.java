6455c7c09e9345564126093c82574ff831648571
Paul Wagener <mail@paulwagener.nl>
2013-12-20 10:51:58 +0100

    Added starting point for Students

---
+++ src/bot/Main.java
@@ -0,0 +1,13 @@
+package bot;
+
+import bombsandberries.GameControl;
+
+public class Main {
+	public static void main(String args[]) {
+		GameControl control = new GameControl("145.102.80.208", "vul hier je studentnummer in");
+		
+		while(true) {
+			// TODO
+		}
+	}
+}
