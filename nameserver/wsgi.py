c569f5881a5702c2472dfd4cd480b1a072121d9e
Paul <mail@paulwagener.nl>
2015-01-07 19:06:03 +0100

    Updated for server

--- nameserver/wsgi.py
+++ nameserver/wsgi.py
@@ -7,8 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nameserver.settings")
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
